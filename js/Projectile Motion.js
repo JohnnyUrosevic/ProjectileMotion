0df4cae3f1b402b660ab9dc268d757dcf7826526
Johnny Urosevic <TheMagicalCake@gmail.com>
2015-07-05 13:45:20 -0700

    Added error message for inputting an incorrect angle
    created calculate function which store input fields into variables
    detects if angle isn't between 90 and 0 and displays text to the canvas

--- js/Projectile Motion.js
+++ js/Projectile Motion.js
@@ -1,5 +1,31 @@
-var canvas = document.getElementById('canvas'),
-ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth / 2;
-canvas.height = window.innerHeight / 2;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth - 15;
+canvas.height = window.innerHeight / 1.1;
 
+ctx.fillStyle='black';
+ctx.fillRect(0,0,canvas.width,canvas.height);
+
+ctx.font = '20px Roboto Condensed'
+ctx.save();
+
+ctx.translate(0, canvas.height);
+ctx.scale(1, -1);
+
+var calculate = function() {		
+	var velocity = parseFloat(document.getElementById('Velocity').value);
+	var angle = parseFloat(document.getElementById('Angle').value);
+	var initialHeight = parseFloat(document.getElementById('Height').value) || 0;
+	var vertPlane = parseFloat(document.getElementById('Vertical').value);
+	var landPlane =  parseFloat(document.getElementById('Final-Height').value) || 0;
+
+	if (angle > 90 || angle < 0 || angle === NaN) {
+		ctx.restore();
+		ctx.save();
+		ctx.fillStyle = 'red';
+		ctx.fillText("Please pick an angle between 0 and 90 degrees", canvas.width / 1.5, canvas.height / 2);
+		ctx.translate(0, canvas.height);
+		ctx.scale(1, -1);
+		
+	}
+}
